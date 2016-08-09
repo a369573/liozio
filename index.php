93eb7e6d3cf39238fd30ce45eaef26698aeb7241
a369573 <a369573@naver.com>
2016-08-09 09:01:41 +0900

    수정합니다.

--- index.php
+++ index.php
@@ -1,4 +1,38 @@
 <?php
-echo "test<br>";
-echo "한글수정한당fffdfdfdfdsddf";
+class Address
+{
+    private $_country;
+
+    public function __construct($country)
+    {
+        $this->_country = $country;
+    }
+
+    public function getCountry()
+    {
+        return $this->_country;
+    }
+}
+
+class Customer
+{
+    private $_name;
+    private $_address;
+
+    public function __construct($name, $country)
+    {
+        $this->_name = $name;
+        $this->_address = new Address($country);
+    }
+
+    public function getName()
+    {
+        return $this->_name();
+    }
+
+    public function getAddress()
+    {
+        return $this->_address();
+    }
+}
 ?>
