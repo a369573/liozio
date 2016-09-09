52330c3463577a5389db2318fd6e8ca20adaec52
a369573 <a369573@naver.com>
2016-09-09 15:41:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,106 @@
 <?php
-class Address
-{
-    private $_country;
-
-    public function __construct($country)
-    {
-        $this->_country = $country;
+echo "<meta charset='utf-8'>";
+/**
+ * Class StrategyContext
+ */
+class StrategyContext {
+    private $strategy = NULL;
+    public function __construct($strategy_ind_id) {
+        switch ($strategy_ind_id) {
+            case "SP":
+                $this->strategy = new serializeP();
+                break;
+            case "USP":
+                $this->strategy = new unserializeP();
+                break;
+            case "EJP":
+                $this->strategy = new jsonEncodeP();
+                break;
+            case "DJP":
+                $this->strategy = new jsonDecodeP();
+                break;
+        }
     }
+    public function processC($input) {
+        return $this->strategy->cording($input);
+    }
+}
 
-    public function getCountry()
-    {
-        return $this->_country;
+interface StrategyInterface {
+    public function cording($input);
+}
+
+class serializeP implements StrategyInterface {
+    public function cording($input) {
+        $r = serialize($input->getVal());
+        return $r;
     }
 }
 
-class Customer
-{
-    private $_name;
-    private $_address;
-
-    public function __construct($name, $country)
-    {
-        $this->_name = $name;
-        $this->_address = new Address($country);
-    }
-
-    public function getName()
-    {
-        return $this->_name();
-    }
-
-    public function getAddress()
-    {
-        return $this->_address();
+class unserializeP implements StrategyInterface {
+    public function cording($input) {
+        $r = unserialize($input->getVal());
+        return $r;
     }
 }
+
+class jsonEncodeP implements StrategyInterface {
+    public function cording($input) {
+        $r = json_encode($input->getVal());
+        return $r;
+    }
+}
+
+class jsonDecodeP implements StrategyInterface {
+    public function cording($input) {
+        $r = json_decode($input->getVal());
+        return $r;
+    }
+}
+
+class mainContain {
+    private $oriVal;
+    private $outVal;
+
+    public function __construct() {
+//        $this->oriVal = $oriVal;
+    }
+
+    public function getVal() {
+        return $this->oriVal;
+    }
+
+    public function setVal($oriVal) {
+        $this->oriVal = $oriVal;
+    }
+
+    public function outVal($output) {
+        $outVal = $output."<br>";
+        echo $outVal;
+    }
+}
+
+
+$input = new mainContain();
+$input->setVal('가나다');
+
+$a = new StrategyContext('SP');
+$b = new StrategyContext('USP');
+$c = new StrategyContext('EJP');
+$d = new StrategyContext('DJP');
+
+$aa = $a->processC($input);
+$input->setVal($aa);
+$bb = $b->processC($input);
+
+$input->setVal('가나다라마버사');
+$cc = $c->processC($input);
+$input->setVal($cc);
+$dd = $d->processC($input);
+
+$input->outVal($aa);
+$input->outVal($bb);
+$input->outVal($cc);
+$input->outVal($dd);
+
 ?>
